77488480f060b6d3814953e0f7648b11be8052a1
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-06-13 22:24:48 +0400

    added UI

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,51 @@
+import { Metadata } from "next"
+import { siteConfig } from "~/config/site"
+import { fontSans } from "~/utils/fonts"
+import { cn } from "~/utils/lib"
+import { SiteHeader } from "~/components/site-header"
+import { TailwindIndicator } from "~/components/tailwind-indicator"
+import { ThemeProvider } from "~/components/theme-provider"
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <>
+        <div
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="relative flex min-h-screen flex-col">
+              <SiteHeader />
+              <div className="flex-1">{children}</div>
+            </div>
+            <TailwindIndicator />
+          </ThemeProvider>
+        </div>
+    </>
+  )
+}
+
+export default RootLayout
